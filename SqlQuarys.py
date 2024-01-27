d09bdb87338330395a50ce8d29a7f7a9071ccb6e
saipa <saipavan861@gmail.com>
2024-01-27 15:56:32 +0530

    init

--- SqlQuarys.py
+++ SqlQuarys.py
@@ -1,9 +1,14 @@
 insertSipetransaction = """
         INSERT INTO ZooSwipeTransactions (superId, BID, ticketType, BRN, GATE, availableAdultEntryCount,
-                                       totalAdultEntryCount, bookingDate, createdOn, isactive)
-        VALUES ({0}, '{1}', '{2}', '{3}', '{4}', {5}, {6}, '{7}', '{8}', 1)
+                                       totalAdultEntryCount, bookingDate, createdOn, isactive,GATETYPE)
+        VALUES ({0}, '{1}', '{2}', '{3}', '{4}', {5}, {6}, '{7}', '{8}', 1,'{9}')
         """
 
 insertHelathcheck = """INSERT INTO ZooHealthStatus (typeid, message, gate, ErrorMessage,createdOn,superid,GateType)
 VALUES ({0}, '{1}', '{2}', '{3}',DATE_ADD(UTC_TIMESTAMP(), INTERVAL '5:30' HOUR_MINUTE),{4},'{5}');
 """
+
+
+gethealthstatusquary = """select * from zoohealthstatus where typeid = {0} 
+and gate = {1} and 
+{2} and GATETYPE = {3}"""
--- db.py
+++ db.py
@@ -19,3 +19,77 @@ def ExecuteUpdate(quary):
         cursor.close()
         connection.close()
 
+import mysql.connector
+
+def ExecuteGetQuery(Quary):
+    ResultModel = {'Status': False, 'Message': None, 'ResultData': []}
+    total_data_to_list = []
+
+    try:
+        try:
+            # Update connection details for MySQL
+            db_connection  = mysql.connector.connect(**db_config)
+            cursor = db_connection.cursor()
+
+            # Execute the query
+            cursor.execute(Quary)
+
+        except mysql.connector.Error as e:
+            ResultModel['Message'] = str(e)
+            ResultModel['Status'] = False
+            ResultModel['ResultData'] = []
+            return ResultModel
+
+        if cursor.with_rows:
+            try:
+                total_data = cursor.fetchall()
+
+                if len(total_data) == 0:
+                    ResultModel['Message'] = 'No Records Found.'
+                    ResultModel['Status'] = False
+                    ResultModel['ResultData'] = total_data_to_list
+
+                    cursor.close()
+                    db_connection.close()
+                    return ResultModel
+
+                for j in range(len(total_data)):
+                    count = 0
+                    dict_data = {}
+
+                    for i in cursor.description:
+                        dict_data[str(i[0]).lower()] = total_data[j][count]
+                        count += 1
+                    total_data_to_list.append(dict_data)
+
+                ResultModel['Status'] = True
+                ResultModel['Message'] = None
+                ResultModel['ResultData'] = total_data_to_list
+
+                cursor.close()
+                db_connection.close()
+                return ResultModel
+
+            except mysql.connector.Error as e:
+                ResultModel['Status'] = False
+                ResultModel['Message'] = str(e)
+                ResultModel['ResultData'] = []
+                cursor.close()
+                db_connection.close()
+                return ResultModel
+
+        else:
+            # Handle the case where the cursor doesn't have rows (e.g., for non-SELECT queries)
+            ResultModel['Status'] = True
+            ResultModel['Message'] = "Query executed successfully."
+            ResultModel['ResultData'] = []
+
+            cursor.close()
+            db_connection.close()
+            return ResultModel
+
+    except mysql.connector.Error as e:
+        ResultModel['Status'] = False
+        ResultModel['Message'] = str(e)
+        ResultModel['ResultData'] = []
+        return ResultModel
